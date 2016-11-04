9554367c04632d34f4c9bc603df97e63406a5c8e
milovanderlinden <milo@dogodigi.net>
2016-11-04 17:39:47 +0100

    Minor correction to let OSRM run

--- eersel-osrm.sh
+++ eersel-osrm.sh
@@ -1,4 +1,4 @@
 docker pull osrm/osrm-backend:v5.4.1
-docker run -t -v $(pwd):/data osrm/osrm-backend:v5.4.1 osrm-extract -p /data/winter.lua /data/eersel.osm
-docker run -t -v $(pwd):/data osrm/osrm-backend:v5.4.1 osrm-contract /data/eersel.osrm
-docker run -t -i -p 5000:5000 -v $(pwd):/data osrm/osrm-backend:v5.4.1 osrm-routed /data/eersel.osrm
+docker run -t -v $(pwd):/data osrm/osrm-backend:v5.4.1 osrm-extract -p /data/dog.lua /data/eersel_noord-brabant.osm
+docker run -t -v $(pwd):/data osrm/osrm-backend:v5.4.1 osrm-contract /data/eersel_noord-brabant.osrm
+docker run -t -i -p 5000:5000 -v $(pwd):/data osrm/osrm-backend:v5.4.1 osrm-routed /data/eersel_noord-brabant.osrm
